68e5602708c9fafd75a63097803e7692d7d8436e
Marina Landisberg, Valuality Technologies Limited Git-Admin <mlandisberg@gmail.com>
2018-03-09 03:58:12 -0800

    Add expense summary to dashboard

---
+++ src/tests/components/ExpensesSummary.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ExpensesSummary } from '../../components/ExpensesSummary.jsx';
+
+test('should correctly render ExpensesSummary with 1 expense', () => {
+
+});
+
+test('should correctly render ExpensesSummary with multiple expenses', () => {
+
+});
