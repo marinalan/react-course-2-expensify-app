3a37e4b8420b2b747d1405822d2e583c7ec9a7db
Marina Landisberg, Valuality Technologies Limited Git-Admin <mlandisberg@gmail.com>
2018-03-24 04:05:13 -0700

    Setup test database env

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -3,9 +3,11 @@ import { shallow } from 'enzyme';
 import { ExpensesSummary } from '../../components/ExpensesSummary.jsx';
 
 test('should correctly render ExpensesSummary with 1 expense', () => {
-
+  const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={235}/>);
+  expect(wrapper).toMatchSnapshot();
 });
 
 test('should correctly render ExpensesSummary with multiple expenses', () => {
-
+  const wrapper = shallow(<ExpensesSummary expenseCount={3} expensesTotal={235}/>);
+  expect(wrapper).toMatchSnapshot();
 });
