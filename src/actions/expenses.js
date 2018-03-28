b323a3830bd861d1ab0262489437700080196f1d
Marina Landisberg, Valuality Technologies Limited Git-Admin <mlandisberg@gmail.com>
2018-03-27 17:27:58 -0700

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,6 +32,14 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }));
+    });
+  };  
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
@@ -46,18 +54,16 @@ export const setExpenses = (expenses) => ({
 
 export const startSetExpenses = () => {
   return (dispatch) => {
-    return database.ref('expenses')
-      .once('value')
-      .then((snapshot) => {
-        const expenses = [];
+    return database.ref('expenses').once('value').then((snapshot) => {
+      const expenses = [];
 
-        snapshot.forEach((childSnapshot) => {
-          expenses.push({
-            id: childSnapshot.key,
-            ...childSnapshot.val()
-          });
+      snapshot.forEach((childSnapshot) => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
         });
-        dispatch(setExpenses(expenses));
       });
-    };
+      dispatch(setExpenses(expenses));
+    });
   };
+};
